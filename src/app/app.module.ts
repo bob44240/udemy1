bd893b1400d871726af9ea900092f2c639250746
Bob <bob@frieden.org>
2017-08-27 00:20:13 -0400

    Add placeholders for basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shoppingLists/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule
